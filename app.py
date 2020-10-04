f6a002e878584903fd929e03b60afdc842f8fab2
mguthrie45 <mguthrie451@gmail.com>
2020-10-04 01:26:46 -0400

    login screen

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def helloWorld():
-    return render_template('index.html')
+def helloWorld(info=None):
+    return render_template('login.html', return_info=info)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def helloName(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/login/', methods=['POST', 'GET'])
+def login_form():
+    users = {"devpsu": 'devpsu'} #username: password
+
+
+    username = request.form.get('username', False)
+    password = request.form.get('password', False)
+    print(username, password)
+
+    first_time = username is False or password is False
+
+    if not first_time:
+        if username not in users:
+            return render_template('login.html', return_info='Username not found')
+        elif users[username] != password:
+            return render_template('login.html', return_info='Incorrect password')
+        else:
+            return redirect(url_for('parks'))
+    else:
+        return render_template('login.html')
 
 @app.route('/parks/')
 def parks():
@@ -19,4 +35,4 @@ def parks():
     return render_template('parks.html', parks=parks)
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=False)
